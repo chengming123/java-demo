93e9c87b1a612b770164345b4a40e74a67ffe6f4
chenming <cm123123>
2021-06-22 19:15:22 +0800

    test

---
+++ javademo/src/algorithmDemo/Demo01.java
@@ -0,0 +1,12 @@
+package algorithmDemo;
+
+
+import static jdk.nashorn.internal.objects.Global.print;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        int a = 5;
+        print(-5);
+    }
+
+}
